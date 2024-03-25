a465a9848f5e79105981a37135648e5da11e1ae4
idyWilliams <williamsidorenyin@yahoo.com>
2024-03-25 14:57:10 +0100

    Header Nav completed

--- src/components/header/DarkModeSwitcher.tsx
+++ src/components/header/DarkModeSwitcher.tsx
@@ -7,7 +7,7 @@ const DarkModeSwitcher = () => {
   return (
     <li>
       <label
-        className={`relative m-0 block h-7.5 w-14 rounded-full ${
+        className={`relative m-0 block h-6 w-11.5 rounded-full ${
           colorMode === "dark" ? "bg-primary" : "bg-stroke"
         }`}
       >
@@ -21,7 +21,7 @@ const DarkModeSwitcher = () => {
           className="dur absolute top-0 z-50 m-0 h-full w-full cursor-pointer opacity-0"
         />
         <span
-          className={`absolute left-[3px] top-1/2 flex h-6 w-6 -translate-y-1/2 translate-x-0 items-center justify-center rounded-full bg-white shadow-switcher duration-75 ease-linear ${
+          className={`absolute left-[3px] top-1/2 flex h-5 w-5 -translate-y-1/2 translate-x-0 items-center justify-center rounded-full bg-white shadow-switcher duration-75 ease-linear ${
             colorMode === "dark" && "!right-[3px] !translate-x-full"
           }`}
         >
--- src/components/header/DropdownUser.tsx
+++ src/components/header/DropdownUser.tsx
@@ -39,30 +39,33 @@ const DropdownUser = () => {
       <Link
         ref={trigger}
         onClick={() => setDropdownOpen(!dropdownOpen)}
-        className="flex items-center gap-4"
+        className="flex items-center gap-2"
         href="#"
       >
         <span className="hidden text-right lg:block">
-          <span className="block text-sm font-medium text-black dark:text-white">
-            Thomas Anree
+          <span className="block text-xs font-light text-body dark:text-white">
+            Hello
           </span>
-          <span className="block text-xs">UX Designer</span>
+          <span className="block text-xs text-body dark:text-white">
+            Williams Idorenyin
+          </span>
         </span>
 
-        <span className="h-12 w-12 rounded-full">
+        <span className="h-8 w-8">
           <Image
-            width={112}
-            height={112}
-            src={"/images/user/user-01.png"}
+            width={100}
+            height={100}
+            src={"/image/williams.jpeg"}
             style={{
               width: "auto",
               height: "auto",
             }}
             alt="User"
+            className="rounded-full"
           />
         </span>
 
-        <svg
+        {/* <svg
           className="hidden fill-current sm:block"
           width="12"
           height="8"
@@ -76,7 +79,7 @@ const DropdownUser = () => {
             d="M0.410765 0.910734C0.736202 0.585297 1.26384 0.585297 1.58928 0.910734L6.00002 5.32148L10.4108 0.910734C10.7362 0.585297 11.2638 0.585297 11.5893 0.910734C11.9147 1.23617 11.9147 1.76381 11.5893 2.08924L6.58928 7.08924C6.26384 7.41468 5.7362 7.41468 5.41077 7.08924L0.410765 2.08924C0.0853277 1.76381 0.0853277 1.23617 0.410765 0.910734Z"
             fill=""
           />
-        </svg>
+        </svg> */}
       </Link>
 
       {/* <!-- Dropdown Start --> */}
